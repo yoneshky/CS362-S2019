ba4c063f1b96230f78c2c768cf6266d4d2208e4f
Kyle <yoneshky@oregonstate.edu>
2019-05-04 21:20:13 -0700

    edits

--- projects/yoneshky/dominion/dominion.c
+++ projects/yoneshky/dominion/dominion.c
@@ -645,21 +645,21 @@ int getCost(int cardNumber)
 }
 
 int adventurerEffect(struct gameState *state) {
-	printf("line 648\n");
+
     int currentPlayer = whoseTurn(state);
     int temphand[MAX_HAND];// moved above the if statement
     int drawntreasure=0;
     int cardDrawn;
     int z = 0;// this is the counter for the temp hand
-	printf("line 654\n");
+
     while(drawntreasure<2){
-		printf("line deck count\n");
+
         if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
             shuffle(currentPlayer, state);
         }
-		printf("draw\n");
+
         drawCard(currentPlayer, state);
-		printf("card drawn\n");
+
         cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
 		if (cardDrawn == copper || (cardDrawn == silver && cardDrawn == gold)) {
             drawntreasure++;
@@ -670,7 +670,7 @@ int adventurerEffect(struct gameState *state) {
             z++;
         }
     }
-		printf("line 669\n");
+
     while(z-1>=0){
         state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
         z=z-1;
