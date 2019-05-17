c12d9c3ab69d838abeac6aed67ab708775447ebc
Kyle <yoneshky@oregonstate.edu>
2019-05-16 21:07:27 -0700

    Update randomtestadventurer.c

--- projects/yoneshky/dominion/randomtestadventurer.c
+++ projects/yoneshky/dominion/randomtestadventurer.c
@@ -15,8 +15,11 @@
 
 int main() {
 	int i, j, n, advPass = 0, testPass = 0, advFail = 0, initTreasure = 0, postTreasure = 0, treasureCount = 0;
+	
 	int k[10] = {adventurer, smithy, council_room, feast, mine, remodel, village, baron, great_hall, minion};
+	
 		printf("Hello");
+	
 	for(n = 0; n < 2000; n++) {
 		int seed = 1000;
 		int numPlayers = rand()%5;
